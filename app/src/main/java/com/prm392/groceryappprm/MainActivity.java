03719a9c0186098dacf6930ba92bdca0a6b6d86f
khanhlqse150838 <khanhlqse150838@fpt.edu.vn>
2023-02-28 20:28:17 +0700

    Login and register UI

--- app/src/main/java/com/prm392/groceryappprm/MainActivity.java
+++ app/src/main/java/com/prm392/groceryappprm/MainActivity.java
@@ -2,7 +2,9 @@ package com.prm392.groceryappprm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view) {
+        startActivity(new Intent(MainActivity.this,LoginActivity.class));
+    }
+
+    public void registration(View view) {
+        startActivity(new Intent(MainActivity.this,RegistrationActivity.class));
     }
 }
--- app/src/main/java/com/prm392/groceryappprm/RegistrationActivity.java
+++ app/src/main/java/com/prm392/groceryappprm/RegistrationActivity.java
@@ -2,13 +2,43 @@ package com.prm392.groceryappprm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+
+    Button signUp;
+    EditText email, name, password;
+    TextView signIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        signUp = findViewById(R.id.login_btn);
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email_reg);
+        password = findViewById(R.id.password_reg);
+        signIn = findViewById(R.id.sign_in);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
