4884ee07203261e5363eaae5547819ebad26f885
hado9 <hado@intelisysaviation.com>
2023-03-01 07:29:16 +0700

    Integrate Login view

--- app/src/main/java/com/prm392/groceryappprm/LoginActivity.java
+++ app/src/main/java/com/prm392/groceryappprm/LoginActivity.java
@@ -8,12 +8,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.prm392.groceryappprm.api.ApiService;
+import com.prm392.groceryappprm.model.User;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button signIn;
     EditText email, password;
     TextView signUp;
+    //ApiService
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +50,30 @@ public class LoginActivity extends AppCompatActivity {
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                loginUser();
             }
         });
+    }
+
+    private void loginUser() {
+        ApiService.apiService
+                .loginUser("abc@gmail.com", "123")
+                .enqueue(new Callback<User>() {
+                    @Override
+                    public void onResponse(Call<User> call, Response<User> response) {
+                        Toast.makeText(LoginActivity.this, "Call API success", Toast.LENGTH_SHORT).show();
+                        User user = response.body();
+                        if (user != null && user.getUserName().trim().length() > 0){
+                            Toast.makeText(LoginActivity.this, "Welcome", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Wrong email or password", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<User> call, Throwable t) {
+                        Toast.makeText(LoginActivity.this, "Call API fail", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
---
+++ app/src/main/java/com/prm392/groceryappprm/api/ApiService.java
@@ -0,0 +1,27 @@
+package com.prm392.groceryappprm.api;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.prm392.groceryappprm.model.User;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiService {
+    //http://localhost:8080/api/customer/login?email=abc@gmail.com&password=123
+    Gson gson = new GsonBuilder()
+            .setDateFormat("yyyy-MM-dd HH:mm:ss")
+            .create();
+    ApiService apiService = new Retrofit.Builder()
+            .baseUrl("http://192.168.1.227:8080/")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+            .create(ApiService.class);
+
+    @GET("api/customer/login")
+    Call<User> loginUser(@Query("email") String email,
+                         @Query("password") String password);
+}
