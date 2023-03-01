e4bb7c88143f3b829321ea9d1e69b2400beca06a
hado9 <hado@intelisysaviation.com>
2023-03-01 21:10:48 +0700

    Integrate register function

--- app/src/main/java/com/prm392/groceryappprm/RegistrationActivity.java
+++ app/src/main/java/com/prm392/groceryappprm/RegistrationActivity.java
@@ -8,6 +8,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.prm392.groceryappprm.api.ApiService;
+import com.prm392.groceryappprm.model.User;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -37,8 +45,36 @@ public class RegistrationActivity extends AppCompatActivity {
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if (name.getText().toString().trim().length() == 0
+                || email.getText().toString().trim().length() == 0
+                || password.getText().toString().trim().length() == 0) {
+                    Toast.makeText(RegistrationActivity.this, "Some fields are empty", Toast.LENGTH_SHORT).show();
+                }
+                registerUser();
             }
         });
+    }
+    private void registerUser() {
+        User user = new User(name.getText().toString(), email.getText().toString(), password.getText().toString());
+        ApiService.apiService
+                .registerUser(user)
+                .enqueue(new Callback<User>() {
+                    @Override
+                    public void onResponse(Call<User> call, Response<User> response) {
+                        Toast.makeText(RegistrationActivity.this, "Call API successfully", Toast.LENGTH_SHORT).show();
+                        User user = response.body();
+                        if (user.getUserName().trim().length() > 0) {
+                            Toast.makeText(RegistrationActivity.this, "Registered successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
+                        } else {
+                            Toast.makeText(RegistrationActivity.this, "User already existed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<User> call, Throwable t) {
+                        Toast.makeText(RegistrationActivity.this, "Call API fail", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
--- app/src/main/java/com/prm392/groceryappprm/model/User.java
+++ app/src/main/java/com/prm392/groceryappprm/model/User.java
@@ -5,6 +5,12 @@ public class User {
     String email;
     String password;
 
+    public User(String userName, String email, String password) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
     public String getUserName() {
         return userName;
     }
