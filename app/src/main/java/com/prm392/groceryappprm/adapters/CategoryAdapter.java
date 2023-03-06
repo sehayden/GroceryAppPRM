9a1aef9c633793270937fc8c67523f08f3696c02
hado9 <hado@intelisysaviation.com>
2023-03-06 23:57:03 +0700

    Integrate category UI

---
+++ app/src/main/java/com/prm392/groceryappprm/adapters/CategoryAdapter.java
@@ -0,0 +1,52 @@
+package com.prm392.groceryappprm.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.prm392.groceryappprm.R;
+import com.prm392.groceryappprm.model.Category;
+
+import java.util.List;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder> {
+    Context context;
+    List<Category> categoryList;
+
+    public CategoryAdapter(Context context, List<Category> categoryList) {
+        this.context = context;
+        this.categoryList = categoryList;
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.nav_cat_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Glide.with(context).load(categoryList.get(position).getImageUrl()).into(holder.imageView);
+        holder.name.setText(categoryList.get(position).getCategoryName());
+    }
+    @Override
+    public int getItemCount() {
+        return categoryList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        TextView name;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.cat_nav_img);
+            name = itemView.findViewById(R.id.nav_cat_name);
+        }
+    }
+}
