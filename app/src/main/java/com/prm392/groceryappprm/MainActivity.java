da2d1d4a94c21dec88324b17e8f41d8b5e74883e
khanhlqse150838 <khanhlqse150838@fpt.edu.vn>
2023-03-11 07:53:03 +0700

    Main activity fix

--- app/src/main/java/com/prm392/groceryappprm/MainActivity.java
+++ app/src/main/java/com/prm392/groceryappprm/MainActivity.java
@@ -19,13 +19,14 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 import com.prm392.groceryappprm.R;
+import com.prm392.groceryappprm.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-    //FirebaseAuth auth;
-    //FirebaseDatabase database;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,29 +35,41 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //auth = FirebaseAuth.getInstance();
-        //database = FirebaseDatabase.getInstance();
-
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_category, R.id.nav_profile, R.id.nav_offers, R.id.nav_new_products,
-                R.id.nav_my_orders, R.id.nav_my_carts, R.id.nav_chat)
+                R.id.nav_home, R.id.nav_category, R.id.nav_profile,R.id.nav_offers,R.id.nav_new_products,
+                R.id.nav_my_orders,R.id.nav_my_carts)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-//
+
 //        View headerView = navigationView.getHeaderView(0);
 //        TextView headerName = headerView.findViewById(R.id.nav_header_name);
 //        TextView headerEmail = headerView.findViewById(R.id.nav_header_email);
 //        CircleImageView headerImg = headerView.findViewById(R.id.nav_header_img);
 
-
+//        database.getReference().child("Users").child(FirebaseAuth.getInstance().getUid())
+//                .addListenerForSingleValueEvent(new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                        UserModel userModel = snapshot.getValue(UserModel.class);
+//
+//                        headerName.setText(userModel.getName());
+//                        headerEmail.setText(userModel.getEmail());
+//                        Glide.with(MainActivity.this).load(userModel.getProfileImg()).into(headerImg);
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(@NonNull DatabaseError error) {
+//
+//                    }
+//                });
 
 
     }
@@ -69,23 +82,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-
-//        if (id == R.id.logout) {
-//            auth.signOut();
-//            startActivity(new Intent(MainActivity.this,HomeActivity.class));
-//            finish();
-//        }
-        return true;
-    }
 
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
-//        return true;
     }
 }
